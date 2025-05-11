f15d92bde69b74632b4fbc128169b9427ea6849c
omgsideburns <91215451+omgsideburns@users.noreply.github.com>
2025-05-11 07:27:53 -0400

    add randomization to photos

--- modules/photos/photos.js
+++ modules/photos/photos.js
@@ -11,11 +11,19 @@ fetch("/api/photos")
   .then(res => res.json())
   .then(photos => {
     photoList = photos;
+    shuffleArray(photoList);
     if (photoList.length) {
       rotatePhoto(); // Start immediately
       setInterval(rotatePhoto, PHOTO_SETTINGS.transitionTime); // Change every transitionTime ms
     }
   });
+
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
 
 function rotatePhoto() {
   const next = photoList[currentPhotoIndex];
